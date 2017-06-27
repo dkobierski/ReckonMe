12b29891495c3e0dfc13097d5c1b5600a31269c6
Mateusz Kleczkowski <wagrav@gmail.com>
2017-06-27 21:09:03 +0200

    translations

--- ReckonMe/ReckonMe/Constants/AccountResponses.cs
+++ ReckonMe/ReckonMe/Constants/AccountResponses.cs
@@ -2,8 +2,13 @@
 {
     public static class AccountResponses
     {
-        public const string UserAlreadyExists = "Given username is already taken by another user.";
-        public const string ConnectionProblem = "There was a problem with connection to authentication server.";
-        public const string InvalidCredentials = "Invalid credentials.";
+        public const string UserAlreadyExists = "Użytkownik o takiej nazwie istnieje już w naszej bazie.";
+            //"Given username is already taken by another user.";
+
+        public const string ConnectionProblem = "Wystąpił problem podczas połączenia z serwerem. Porsimy spróbuj ponownie później.";
+            //"There was a problem with connection to authentication server.";
+
+        public const string InvalidCredentials = "Padane hasło nie jes właściwe"; 
+            //"Invalid credentials.";
     }
 }
